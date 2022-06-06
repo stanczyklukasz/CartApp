73cfb327d274ef67e04264830984e5c0a5a6c5a4
stanczykl <stanczyk.lukasz@outlook.com>
2022-06-06 20:38:57 +0200

    added simple docker

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
+
+use App\Shared\Infrastructure\SymfonyKernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new SymfonyKernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- src/Shared/Infrastructure/SymfonyKernel.php
+++ src/Shared/Infrastructure/SymfonyKernel.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App;
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+class SymfonyKernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
